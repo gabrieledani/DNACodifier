e97c76eee47ff7b04db68880000882c4a100c29b
Gabriele Dani <gdani@ucs.br>
2021-12-01 22:22:08 -0300

    heroku

--- DNACodifier/settings.py
+++ DNACodifier/settings.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 import os.path
 
+import django_heroku
+import dj_database_url
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +41,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'DNACodifier.urls'
@@ -114,7 +119,12 @@ STATICFILES_DIRS = [
 
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+django_heroku.settings(local())
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn DNACodifier.wsgi
